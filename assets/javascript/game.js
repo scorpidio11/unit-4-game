e7e66e1db17159557dd982e4cb293259d2cf5ffa
scorpidio11 <scorpido@gmail.com>
2018-08-22 14:37:27 -0700

    link to game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,221 @@
 
-    // VARIABLES
-      // ==========================================================================
-    var wins = 0;
-    var loses = 0;
-    var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-   
-    //var computerChoices = "abcdefghijklmnopqrstuvwxyz";
-    var guessLeft = 9;
 
-    var userGuessTotal = [];
-    //var computerGuessTotal = [];
+$(document).ready(function () {
 
-    // var reset = function () {
-    //     document.location.reload(true);
+    var targetNumber = Math.floor(Math.random() * 120 + 19); // random traget number 19-120
+    var bt1 = Math.floor(Math.random() * 12 + 1);
+    var bt2 = Math.floor(Math.random() * 12 + 1);
+    var bt3 = Math.floor(Math.random() * 12 + 1);
+    var bt4 = Math.floor(Math.random() * 12 + 1);
 
-    // }
 
-     // FUNCTIONS
-      // ==============================================================================
-    document.onkeyup = function (event) {
-        var userGuess = event.key.toLowerCase();
-        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    
-      //Guessed Alpahet added to empty arry and show on screen
-        userGuessTotal.push(userGuess);
-      
+    // var crystalRandom = Math.floor(Math.random()*12+1);
 
-        if (userGuess == computerGuess){
-            wins++;
-            guessLeft = 9;
-            userGuessTotal.length = 0;
+    var total = 0;
+    var win = 0;
+    var lose = 0;
+    var message = "";
+
+
+
+    // 1. random number 19-120 show on the page on load & Apped random numbers to Crystal
+
+    $("#targetNum").text(targetNumber);
+
+
+    // <button1>
+
+    //Event delegation    $(document).on("click", "#button1", function () 
+
+    $("#button1").on("click", function () {
+
+
+        $("#button1").append(bt1);
+        console.log(bt1);
+
+        total += bt1;
+        $("#totalNum").text(total);
+
+        if (total === targetNumber) {
+            $("#message").text("YOU WON");
+            $("#win").text(++win);
+
+
+            // re assigning button do not use var  #
+                $("#button1").empty();
+            bt1 = Math.floor(Math.random() * 12 + 1);
+            $("#button1").append(bt1);
+
+
+            reset();
+            console.log(bt2);
+
         }
 
-        else if  (guessLeft == 0)
-        
-        {
-            loses++;
-            guessLeft = 9;
-            userGuessTotal.length = 0;
-          
+        else if (total > targetNumber) {
+            $("#message").text("YOU LOST");
+            $("#lose").text(++lose);
+
+
+
+               // re assigning button #
+            $("#button1").empty();
+            bt1 = Math.floor(Math.random() * 12 + 1);
+            $("#button1").append(bt1);
+            reset();
+        }
+    });
+
+
+
+    // <button2>
+
+    $("#button2").on("click", function () {
+
+
+        $("#button2").append(bt2);
+        console.log(bt2);
+
+        total += bt2;
+        $("#totalNum").text(total);
+
+
+
+        if (total === targetNumber) {
+            $("#message").text("YOU WON");
+            $("#win").text(++win);
+
+
+
+// re assigning button #
+            $("#button2").empty();
+            bt2 = Math.floor(Math.random() * 12 + 1);
+            $("#button2").append(bt2);
+            reset();
         }
 
-        else if (userGuess !== computerGuess){
-            guessLeft--; //decrementing the guesses left
-        }  
-
-      
+        else if (total > targetNumber) {
+            $("#message").text("YOU LOST");
+            $("#lose").text(++lose);
 
 
-        // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
-        var html =
+// re assigning button #
+
+            $("#button2").empty();
+            bt2 = Math.floor(Math.random() * 12 + 1);
+            $("#button2").append(bt2);
+            reset();
+        }
+
+    });
 
 
-            "<p>Wins: " + wins + "</p>" +
-            "<p>Losses: " + loses + "</p>" +
-            "<p>Guesses Left: " + guessLeft + "</p>" +
-            // "<p>Computer Guesses so far:  " + computerGuessTotal + "</p>" +
-            "<p>Your Guesses so far: " + userGuessTotal + "</p>";
+    // <button3>
 
-        // Set the inner HTML contents of the #game div to our html string
-        document.querySelector("#game").innerHTML = html;
-  
-    };
+    $("#button3").on("click", function () {
+
+
+        $("#button3").append(bt3);
+        console.log(bt3);
+
+        total += bt3;
+        $("#totalNum").text(total);
+
+
+        if (total === targetNumber) {
+            $("#message").text("YOU WON");
+            $("#win").text(++win);
+
+
+            reset();
+
+            $("#button3").empty();
+            bt3 = Math.floor(Math.random() * 12 + 1);
+            $("#button3").append(bt3);
+
+        }
+
+        else if (total > targetNumber) {
+            $("#message").text("YOU LOST");
+            $("#lose").text(++lose);
+
+
+            reset();
+            $("#button3").empty();
+            bt3 = Math.floor(Math.random() * 12 + 1);
+            $("#button3").append(bt3);
+        }
+
+    });
+
+    // <button4>
+
+
+    $("#button4").on("click", function () {
+
+
+        $("#button4").append(bt4);
+        console.log(bt4);
+
+        total += bt4;
+        $("#totalNum").text(total);
+
+
+
+        if (total === targetNumber) {
+            $("#message").text("YOU WON");
+            $("#win").text(++win);
+
+            reset();
+            $("#button4").empty();
+            bt4 = Math.floor(Math.random() * 12 + 1);
+            $("#button4").append(bt4);
+        }
+
+        else if (total > targetNumber) {
+            $("#message").text("YOU LOST");
+            $("#lose").text(++lose);
+
+
+            reset();
+            $("#button4").empty();
+            bt4 = Math.floor(Math.random() * 12 + 1);
+            $("#button4").append(bt4);
+        }
+
+    });
+
+
+
+    function reset() {
+
+        $("#totalNum").empty();
+        total = 0;
+        $("#totalNum").text(total);
+
+        console.log(total);
+
+        $("#targetNum").empty();
+        targetNumber = Math.floor(Math.random() * 120 + 19);
+
+
+        $("#targetNum").text(targetNumber);
+        console.log(targetNumber);
+
+    }
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
